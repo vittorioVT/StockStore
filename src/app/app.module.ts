4c1b8d4dc62a32b236632367b30fc863a1e6d080
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-11-01 13:58:57 +0200

    Added new-product.component for POST-method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { NewProductComponent } from './new-product/new-product.component';
+
 //services
 import { ProductService } from './product.service';
 import { AppRouterModule } from './app-router.module';
 import { HttpClientModule } from '@angular/common/http';
+
 //material design
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import { MatTableModule } from '@angular/material/table';
+
 
 
 
@@ -22,7 +26,8 @@ import { MatTableModule } from '@angular/material/table';
     AppComponent,
     ProductsComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
