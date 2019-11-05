f4ef529176fd8a85152a2095937c3014264966b6
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-11-05 14:50:06 +0200

    Added register() and login() methods

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  baseUrl: string = 'http://localhost:13764/api/auth';
+
+  constructor(private http: HttpClient) { }
+
+  register(user) {
+    return this.http.post(this.baseUrl + 'register', user);
+  }
+
+  login(user) {
+    return this.http.post(this.baseUrl + 'login', user);
+  }
+
 }
