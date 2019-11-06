96dbd318b857b9b977c1aba361cab0545169b1f1
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-11-06 13:00:37 +0200

    Added function matchingFields() for validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup; 
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: matchingFields('password', 'confirmPassword')
+    })
+  }
+
+  
   ngOnInit() {
   }
 
+}
+
+function matchingFields(field1, field2) {
+  return form => {
+    if (form.controls[field1].value !== form.controls[field2].value)
+      return { matchingFields: true }
+
+  }
 }
